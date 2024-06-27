40bb12780af2cf462dfc39354901171515010ae1
Randal Wang <rf.wangchn@foxmail.com>
2024-06-28 00:00:05 +0800

    feat: Add `About` doc file and remove unused doc files

--- src/components/Button/button.stories.ts
+++ src/components/Button/button.stories.ts
@@ -3,7 +3,7 @@ import { Button } from '../../';
 import type { Meta, StoryObj } from '@storybook/react';
 
 const meta = {
-  title: 'Custom/Button',
+  title: 'General/Button',
   component: Button,
   argTypes: {},
 } satisfies Meta<typeof Button>;
